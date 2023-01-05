c36493e7d81fc4c16457b3d7ecbfd7a85a4ddb26
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-01-04 21:21:45 -0600

    Cambios

--- components/ui/Cuadro/index.tsx
+++ components/ui/Cuadro/index.tsx
@@ -1,15 +1,32 @@
-import { FC } from 'react';
-import styles from './styles.module.css';
+import { FC, DragEvent } from 'react';
+import { motion } from 'framer-motion'
+import styles from './styles.module.css'
 
 interface Props {
   children?: JSX.Element;
   color: string;
+  id: string;
 }
 
-export const Cuadro: FC<Props> = ({ color, children }) => {
+export const Cuadro: FC<Props> = ({ color, children, id }) => {
+
+  const onDrop = (e:DragEvent<HTMLDivElement>) => {
+    console.log(e)
+  }
+
+  const onDragOver = (e:DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className={styles.cuadro} style={{ backgroundColor: color }}>
+    <motion.div
+      id={id}
+      className={styles.cuadro}
+      style={{ backgroundColor: color }}
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+    >
       {children}
-    </div>
+    </motion.div>
   )
 }
--- components/ui/Tablero/index.tsx
+++ components/ui/Tablero/index.tsx
@@ -7,28 +7,27 @@ import { Cuadro } from "../Cuadro";
 
 import styles from './styles.module.css';
 
-import { tablero, obtenerImagen } from '../../../utils';
+import { obtenerImagen } from '../../../utils';
 
-const filas = [1, 2, 3, 4, 5, 6, 7, 8];
-const columnas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-
+const gameClient = chess.create();
+let move;
+let status = gameClient.getStatus();
 
 export const Tablero = () => {
-
-    const gameClient = chess.create();
-    let move, status;
-    status = gameClient.getStatus();
-
     console.log(status);
-
     return (
         <div className={styles.tablero}>
             {
                 [...status.board.squares].reverse().map((square, i) => (
-                    <Cuadro key={i} color={(i + square.rank ) % 2 === 0 ? '#fff' : '#000'}>
+                    <Cuadro 
+                        id={`${square.file}${square.rank}`} 
+                        key={i}
+                        color={(i + square.rank ) % 2 === 0 ? '#fff' : '#000'}
+                        >
                         {
                             square.piece ?
                                 <motion.img
+                                    id={`${square.piece.type}-${square.piece.side.name}`}
                                     className={styles.img}
                                     src={obtenerImagen(square.piece.type, square.piece.side.name)}
                                     alt=""
